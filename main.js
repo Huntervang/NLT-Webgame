9a519c8ab863e641d27af39d84b1d25fea3dcfb0
smauwie <huntervangeffen@spinozalyceum.nl>
2015-03-02 16:50:41 +0100

    Added stuff

--- main.js
+++ main.js
@@ -2,20 +2,35 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv',{preload: preload, c
 
 
 function preload(){
-    game.load.spritesheet('ene', 'assets/Enemy.png', 128, 128);
+    game.load.spritesheet('enemy', 'assets/Enemy.png', 128, 128);
+    game.load.spritesheet('player', 'assets/Player.png', 64, 64);
+    game.load.image('background', 'assets/Background.png');
+    game.load.spritesheet('explosion', 'assets/Explosion.png', 32, 32);
+
     game.stage.backgroundColor = '#2e628e';
 }
+
+var player;
+var enemy;
 
 function create(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.enemy = this.game.add.sprite(300, 200, 'ene');
+    starfield = game.add.tileSprite(0, 0, 800, 600, 'background');
 
-    this.enemy.animations.add('fire');
-    this.enemy.animations.play('fire', 10, true);
+    explosion = game.add.sprite(100, 500, 'explosion');
+    explosion.animations.add('explosion');
+    explosion.animations.play('explosion', 10, true);
+
+    player = game.add.sprite(400, 500, 'player');
+    player.animations.add('fire_p');
+    player.animations.play('fire_p', 10, true);
+
+    enemy = game.add.sprite(300, 200, 'enemy');
+    enemy.animations.add('fire');
+    enemy.animations.play('fire', 10, true);
 }
 
 function update(){
 
 }
-
