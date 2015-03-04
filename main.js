0c148ef9388672330ae9f797e0f41c0bac878345
Kuunnn <koen2mail@gmail.com>
2015-03-04 12:02:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,9 @@ function create(){
     player.animations.add('fire_p');
     player.animations.play('fire_p', 10, true);
     player.anchor.setTo(0.5,0.5);
-
+    player.body.setSize(400, 500, 64, 64);
+    player.body.collideWorldBounds = true;
+    player.body.bounce.setTo(0.5, 0.5)
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     bullets = game.add.group(); 
@@ -54,6 +56,9 @@ function create(){
     enemy = game.add.sprite(300, 200, 'enemy');
     enemy.animations.add('fire');
     enemy.animations.play('fire', 10, true);
+    enemy.body.setSize(300,200, 128, 128);
+    enemy.body.collideWorldBounds = true;
+    enemy.body.bounce.setTo(0.5, 0.5)
     game.physics.enable(enemy, Phaser.Physics.ARCADE);
 
     cursor = game.add.sprite(game.input.mousePointer.worldX, game.input.mousePointer.worldY, 'cursor');
