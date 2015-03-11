336b3575a5ea98c4304a22fa755036a507950108
Harry <koen2mail@gmail.com>
2015-03-11 09:35:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ function create(){
     
     for (var i = 0; i < 4; i++){
         astroids.create(game.rnd.integerInRange(400, 1500), game.rnd.integerInRange(400, 1200), 'astroid');
-        astroids.children()[i].immovable = true;
+        astroids.children[i].immovable = true;
     }
 
     explosion = game.add.group();
