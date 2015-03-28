0ff9e59ebf119fc56e8da45aaea760219c415183
Harry <koen2mail@gmail.com>
2015-03-28 17:41:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ var menu = 0;
 
 var enemyBullet;
 var livingEnemies = [];
-var firingTimerEnemy;
+var firingTimerEnemy = 0;
 
 function create(){
     game.world.setBounds(0,0,4000,2000);
@@ -313,7 +313,7 @@ function enemyFires () {
     {
         
         for(var i = 0; i < enemies.length; i++) {
-        if (game.physics.arcade.distanceBetween(player, enemies.children[i]) < 400 /*&& firingTimerEnemy < game.time.now */){
+        if (game.physics.arcade.distanceBetween(player, enemies.children[i]) < 400 && firingTimerEnemy < game.time.now ){
     
 
 
@@ -322,7 +322,7 @@ function enemyFires () {
         enemyBullet.reset(shooter.body.x, shooter.body.y);
 
         game.physics.arcade.moveToObject(enemyBullet,player,120);
-        //firingTimerEnemy = game.time.now + 500;
+        firingTimerEnemy = game.time.now + 500;
     
         }
         }
