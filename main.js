df532e21378fbde20f33ea855b074a6ffead4422
jonasstappers <jsstappers020@gmail.com>
2015-03-29 21:25:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -294,7 +294,7 @@ function aI(enemy) {
     for(var i = 0; i < enemies.length; i++) {
         if (game.physics.arcade.distanceBetween(player, enemies.children[i]) < 400){
             game.physics.arcade.moveToObject(enemies.children[i], player)
-            enemies.children[i].rotation = game.physics.arcade.angleToXY(enemies.children[i], player.x, player.y) + 10.8;
+            enemies.children[i].rotation = game.physics.arcade.angleToXY(enemies.children[i], player.x, player.y) + 190.8;
         }
     }
 }
